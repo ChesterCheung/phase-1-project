d0ebb10a87cce308e865930cb01afde23055ed40
Chester <cheung319@gmail.com>
2022-01-21 00:26:55 -0500

    working on text input for budget

--- phase-1-project/javascripts/index.js
+++ phase-1-project/javascripts/index.js
@@ -1,12 +1,14 @@
 // Node Getters //
 const mainDiv = document.getElementById('main')
 const homeLink = document.getElementById('home-link')
+
 // Event Listeners //
 const attachHomePageLinkEvent = () =>{
     homeLink.addEventListener("click", loadHome);
 }
 // Event Handlers //
 const loadHome = () => {
+    resetHome()
     const h1 = document.createElement('h1')
     const p = document.createElement('p')
     h1.className = 'center-align';
@@ -18,9 +20,11 @@ mainDiv.appendChild(h1);
 mainDiv.appendChild(p);
 }  
 
+// Miscellaneous //
+const resetHome = () => mainDiv.innerHTML = " ";
 
 // Startup //
 document,addEventListener('DOMContentLoaded', function(){
-loadHome()
+// loadHome()
 attachHomePageLinkEvent()
 })
